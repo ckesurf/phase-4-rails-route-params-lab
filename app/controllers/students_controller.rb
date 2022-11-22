6c162f9dab9ca6af8cec9db506812f22d9e6db95
Chris Erlendson <ckesurf@gmail.com>
2022-11-22 11:00:56 -0500

    solution with bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.search(params[:name])
     render json: students
+  end
+
+  def show
+    # Student.find(1, 2, 3) => gives an array
+    render json: Student.find_by(id: params[:id]) # => only one
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
+
+  # http 'path',   to: 'controller#method'
+  get '/students/:id', to: 'students#show'
+
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
 end
