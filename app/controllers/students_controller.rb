b7d5604276ae2d39ba2d1b51da392aba7f737c82
Chris Erlendson <ckesurf@gmail.com>
2023-01-30 17:46:17 -0500

    final code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,30 @@
 class StudentsController < ApplicationController
 
+  # everything
   def index
     students = Student.all
+    # if there's a query param called name,
+    
+    if params[:name]
+      # then only return students with that name
+      search_name = params[:name].downcase
+      students = students.filter do |student| 
+        student.first_name.downcase == search_name || student.last_name.downcase == search_name
+      end
+
+      # Student.where("first_name = ? OR last_name = ?", params[:name])
+
+    end
     render json: students
   end
+
+  # one thing
+  def show
+    student = Student.find(params[:id])
+    render json: student
+
+  end
+
+  
 
 end
